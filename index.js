ef6ccab2f316bb84ab75757a86dfb7d30086c70b
Michelle Patiño <thocokun@gmail.com>
2021-07-09 22:04:12 -0500

    se modifico el puerto

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 const api = new WooCommerceRestApi({
   url: "http://bordados.noobhuman.ninja",
